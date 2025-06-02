45b907143a80c3f12e47af25408151e9ba6f9891
sujeong165 park <sujr110026@gmail.com>
2025-06-02 03:38:25 +0000

    06.02_내 포폴 홈피 만들기03

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ const observer = new IntersectionObserver(entries => {
 document.querySelectorAll('.fade-in').forEach(section => {
   observer.observe(section);
 });
+
+// script.js
+window.addEventListener('scroll', () => {
+  const nav = document.getElementById('mainNav');
+  if (window.scrollY > 20) {
+    nav.classList.add('scrolled');
+  } else {
+    nav.classList.remove('scrolled');
+  }
+});
